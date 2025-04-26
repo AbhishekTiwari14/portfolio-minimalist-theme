89f44777f59293ed91a6d445dd6bee2106236fea
AbhishekTiwari14 <abhishek.tiwari2003@gmail.com>
2025-04-26 09:06:58 +0530

    attempting to put data file in dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import MinimalistTheme from "./components/Minimalist Theme/MinimalistTheme"
-import data from "./data/portfolio-data.json"
 
 function App() {
-  const { fullName } = data
+  const [fullName, setFullName] = useState("Abhishek Tiwari")
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => setFullName(data.fullName))
+      .catch((error) => {
+        console.error("Couldn't load data.json, using fallback data", error)
+      })
+  }, [])
   const firstName = fullName.split(" ")[0]
   useEffect(() => {
     document.title = `${firstName}'s Portfolio`
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return <MinimalistTheme />
